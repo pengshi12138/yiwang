4d93e617fa94a8a860b240aa5c041f97818c8d2b
pengshi12138 <2280650617@qq.com>
2023-05-31 12:34:36 +0800

    完成毕业照部分

--- source/data/毕业照.js
+++ source/data/毕业照.js
@@ -1,60 +1,138 @@
 const imgData = [
   {
-      "height": 160,
-      "compression": "https://t7.baidu.com/it/u=3676218341,3686214618&fm=193&f=GIF",
-      "width": 263,
-      "original": "https://t7.baidu.com/it/u=3676218341,3686214618&fm=193&f=GIF",
-      "title": "人生",
-      "description": "不过如此"
+    "height": 400,
+    "compression": "/images/毕业照/DSC_1088.JPG",
+    "width": 500,
+    "original": "/images/毕业照/DSC_1088.JPG",
+    "title": "毕业照",
+    "description": "四年后离别"
   },
   {
-      "height": 185,
-      "compression": "https://img1.baidu.com/it/u=1089887766,3281198274&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=729",
-      "width": 150,
-      "title": "人生",
-      "description": "不过如此",
-      "original": "https://img1.baidu.com/it/u=1089887766,3281198274&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=729"
+      "height": 550,
+      "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1052.jpg",
+      "width": 500,
+      "original": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1052.jpg",
+      "title": "友谊三剑客",
+      "description": "傻逼伟杰、帅气彭川、智慧科宏"
   },
   {
-      "height": 160,
-      "compression": "https://img2.baidu.com/it/u=1128216298,2207674418&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500",
-      "width": 272,
-      "title": "人生",
-      "description": "不过如此"
+      "height": 540,
+      "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1054.jpg",
+      "width": 500,
+      "title": "软工4班情谊",
+      "description": "好友请记得我",
+      "original": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1054.jpg"
   },
   {
-      "height": 160,
-      "compression": "https://img0.baidu.com/it/u=2401186638,635202548&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500",
-      "width": 272,
-      "title": "人生",
-      "description": "不过如此"
+      "height": 560,
+      "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1057.jpg",
+      "width": 500,
+      "title": "616与617的故事",
+      "description": "左青龙 右白虎"
   },
   {
-      "height": 160,
-      "compression": "https://img0.baidu.com/it/u=1214356167,1289705410&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500",
-      "width": 271,
-      "title": "人生",
-      "description": "不过如此"
+      "height": 545,
+      "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1062.jpg",
+      "width": 500,
+      "title": "616与617的故事",
+      "description": "华农F4"
+  },
+  {
+      "height": 500,
+      "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1065.jpg",
+      "width": 500,
+      "title": "横跨年纪的故事",
+      "description": "大四毕业生和大二学弟的宿舍友谊"
   },
   {
     "height": 3712,
-    "compression": "/images/毕业照/DSC_1088.JPG",
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1068.jpg",
     "width": 5568,
-    "title": "人生",
-    "description": "不过如此"
+    "title": "毕业照学士服",
+    "description": "看看端正毕业生"
   },
   {
     "height": 3712,
-    "compression": "/images/毕业照/DSC_1088.JPG",
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1112.jpg",
     "width": 5568,
-    "title": "人生",
-    "description": "不过如此"
+    "title": "两个肥猪的故事",
+    "description": "左边150斤 右边170斤"
   },
   {
     "height": 3712,
-    "compression": "/images/毕业照/DSC_1088.JPG",
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1117.jpg",
     "width": 5568,
-    "title": "人生",
-    "description": "不过如此"
+    "title": "秋哥与汉勇",
+    "description": "2023年 5 月29号 他们都走了 都去深圳了"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1136.jpg",
+    "width": 5568,
+    "title": "秋哥与pc",
+    "description": "good friend"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1139.jpg",
+    "width": 5568,
+    "title": "拥抱肥猪",
+    "description": "快点减肥吧 富哥"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1142.jpg",
+    "width": 5568,
+    "title": "两白一黑",
+    "description": "好组合"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1145.jpg",
+    "width": 5568,
+    "title": "两白一黑",
+    "description": "好组合"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1142.jpg",
+    "width": 5568,
+    "title": "黑白相间",
+    "description": "友谊长存"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1152.jpg",
+    "width": 5568,
+    "title": "躺平",
+    "description": "好躺"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1166.jpg",
+    "width": 5568,
+    "title": "好 实在是好",
+    "description": "。。。。"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1171.jpg",
+    "width": 5568,
+    "title": "天黑请闭眼",
+    "description": "两眼两睁一闭"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1178.jpg",
+    "width": 5568,
+    "title": "想不出了",
+    "description": "好 妙啊"
+  },
+  {
+    "height": 3712,
+    "compression": "https://pengshipic.4everland.store/pic/graduation_photo/DSC_1184.jpg",
+    "width": 5568,
+    "title": "泰坦尼克号----华农篇",
+    "description": "拥抱我别放手"
   },
 ]
--- themes/edinburgh/source/js/masonry.js
+++ themes/edinburgh/source/js/masonry.js
@@ -7,18 +7,23 @@ class Masonry {
       COLUMN_WIDTH = 500,   // 元素宽度
       GAP_WIDTH = 15,       // 元素水平间距
       GAP_HEIGHT = 15,      // 元素垂直间距
+      WINDOW_WIDTH = 500,   // 浏览器窗口大小
     } = options
 
     this.COLUMN_WIDTH = COLUMN_WIDTH
     this.GAP_WIDTH = GAP_WIDTH
     this.GAP_HEIGHT = GAP_HEIGHT
-
+    if (document.body.offsetWidth <= 500) {
+      this.COLUMN_WIDTH = document.body.offsetWidth - 2 * this.GAP_WIDTH
+      this.WINDOW_WIDTH = document.body.offsetWidth
+      $(".cell").css({"width": this.COLUMN_WIDTH + "px"})
+    }
     this.container = container
     this.columnCount = 0  // 列数
     this.columnHeights = [] // 每列的高度组成的数组
   }
 
-  init(viewer, baseUrl) {
+  init(viewer, baseUrl) { 
     this.ROOT_URL = baseUrl
     this.viewer = viewer
     this.resetColumnCount()
@@ -44,6 +49,8 @@ class Masonry {
     this.loadMoreCells()
   }
 
+ 
+
   bindEvent() {
     // 重新判断形状
     let resizeDelay = null
@@ -53,20 +60,39 @@ class Masonry {
     }
   }
 
+  /* 滚动加载函数 */
+  lozyloading(cells, images) {
+    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    const winHeight = window.innerHeight;
+    let flag = true;
+    for( let i = 0; i < cells.length; i++) {
+      if(cells[i].offsetTop < scrollTop + winHeight && images[i].getAttribute('src') == null) {
+        images[i].src = images[i].getAttribute('data-src');
+        flag = false;
+      }
+    }
+    if(!flag){
+      // 进行实时动态刷新
+      this.viewer.update()
+    }
+  }
+
   async loadMoreCells() {
     const cells = await this.appendCells()
     this.adjustCells(cells)
+    const images = document.querySelectorAll('img');
+    this.lozyloading(cells, images);
+    window.onscroll  = () => {
+       this.lozyloading(cells, images)
+    }
   }
 
   async appendCells() {
     const data = await this.fetchData()
-    
     const fragment = document.createDocumentFragment()
     const cells = []
-
     data.forEach((item, index) => {
         const cell = document.createElement('div')
-        console.log()
         if (item.original != null && String(item.original).indexOf("http") < 0) {
           item.original = this.ROOT_URL + item.original
         }
@@ -84,14 +110,14 @@ class Masonry {
                 id="img_${index}"
                 class="imagebox"
                 data-original="${item.original}" 
-                src="${item.compression}" 
+                data-src="${item.compression}" 
                 width="${this.COLUMN_WIDTH}" 
                 alt="${item.title}"
                 height="${item.height * this.COLUMN_WIDTH / item.width}" 
                 loading="lazy"
                 >
               </div>
-              <div class="text-box">
+              <div class="text-box" id="text_${index}">
                 <div>
                   <h2>${item.title}</h2>
                   <p>${item.description}</p>
@@ -108,12 +134,10 @@ class Masonry {
               <span></span>
             </div>
         `
-        
         cell.style.width = this.COLUMN_WIDTH + "px"
         cell.style.height = item.height * this.COLUMN_WIDTH / item.width + "px"
         cells.push(cell)
         fragment.appendChild(cell)
-        
     })
     
 
@@ -121,7 +145,7 @@ class Masonry {
     // 绑定监听函数进行动画图片加载
     var timer = setInterval(function(){
       for(var index = 0; index < cells.length; ++index) {
-        if (document.getElementById('img_' + index).complete){
+        if (document.getElementById('img_' + index).complete && document.getElementById('img_' + index).getAttribute("src") != null){
           this.PIC_LOADING_NUM ++;
           document.getElementById('loading_' + index).setAttribute("hidden", true)
         }
@@ -129,8 +153,7 @@ class Masonry {
       if (this.PIC_LOADING_NUM == cells.length - 1) {
         clearInterval(timer);
       }
-    }, 100);
-
+    }, 300);
 
     // 进行实时动态刷新
     this.viewer.update()
@@ -149,11 +172,22 @@ class Masonry {
   }
 
   reflowCells() {
-    this.resetColumnCount()
-    this.resetHeights()
-    this.adjustCells(Array.from(this.container.children))
-    // 进行实时动态刷新
-    this.viewer.update()
+    if (document.body.offsetWidth != this.WINDOW_WIDTH) {
+      
+      if (document.body.offsetWidth <= 500) {
+        this.COLUMN_WIDTH = document.body.offsetWidth - 2 * this.GAP_WIDTH;
+      } else {
+        this.COLUMN_WIDTH = 500;
+      }
+      // 充值cell 变量的width
+      $(".cell").css({"width": this.COLUMN_WIDTH + "px"});
+      this.WINDOW_WIDTH = document.body.offsetWidth
+      this.resetColumnCount()
+      this.resetHeights()
+      this.adjustCells(Array.from(this.container.children))
+      // 进行实时动态刷新
+      this.viewer.update()
+    }
   }
 
   // fetchData() {
